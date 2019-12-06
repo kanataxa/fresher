db1ce0dfc6ea9ef7533c6555c77dc304c756f8cf
kanataxa <sota.corei324@gmail.com>
2019-12-06 21:19:47 +0900

    use flags

--- cmd/fresher/main.go
+++ cmd/fresher/main.go
@@ -3,12 +3,38 @@ package main
 import (
 	"log"
 
+	"github.com/jessevdk/go-flags"
 	"github.com/kanataxa/fresher"
 )
 
-func main() {
-	fr := fresher.New()
+type Option struct {
+	Config string `long:"config" short:"c" default:"fresher.yaml" description:"config yaml file name"`
+}
+
+var opts Option
+
+func run() error {
+	c, err := fresher.LoadConfig(opts.Config)
+	if err != nil {
+		return err
+	}
+	fr := fresher.New(c.Options()...)
 	if err := fr.Watch(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	parser := flags.NewParser(&opts, flags.Default)
+	if _, err := parser.Parse(); err != nil {
+		if e, ok := err.(*flags.Error); ok {
+			if e.Type == flags.ErrHelp {
+				return
+			}
+		}
+	}
+	if err := run(); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/goccy/go-yaml v1.1.2
+	github.com/jessevdk/go-flags v1.4.0 // indirect
 	golang.org/x/sys v0.0.0-20191204072324-ce4227a45e2e // indirect
 )
